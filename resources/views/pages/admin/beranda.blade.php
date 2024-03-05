3db89bc3743ba00d17f78e8f45bfd898df4338a9
FerdiNurrahman <ferdi.nurrahman@mhs.politala.ac.id>
2024-03-05 14:41:02 +0800

    Fix View Beranda

--- resources/views/pages/admin/beranda.blade.php
+++ resources/views/pages/admin/beranda.blade.php
@@ -86,7 +86,7 @@
                     <div class="col-12">
                         <div class="card">
                             <div class="card-header">
-                                <h4>Profile Visit</h4>
+                                <h4>Permohonan Bulanan</h4>
                             </div>
                             <div class="card-body">
                                 {!! $chart->container() !!}

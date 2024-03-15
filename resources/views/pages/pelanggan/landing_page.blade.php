d9db58385efd88ef0dbd6769f035e7a75c51aa95
FerdiNurrahman <ferdi.nurrahman@mhs.politala.ac.id>
2024-03-15 11:40:48 +0800

    Perubahan di landing page

--- resources/views/pages/pelanggan/landing_page.blade.php
+++ resources/views/pages/pelanggan/landing_page.blade.php
@@ -11,7 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Title -->
-    <title>Mediplus - Free Medical and Doctor Directory HTML Template.</title>
+    <title>SI HUMAS</title>
 
     <!-- Favicon -->
     <link rel="icon" href="template_landing_page/img/favicon.png">
@@ -199,7 +199,7 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="section-title">
-                        <h2>Langkah-langkah melakukan permohon</h2>
+                        <h2>Langkah-langkah melakukan Permohonan</h2>
 
                     </div>
                 </div>
@@ -211,8 +211,8 @@
                         <div class="signle-icon">
                             <i class="icofont icofont-ambulance-cross"></i>
                         </div>
-                        <h3>Emergency Help</h3>
-                        <p>Lorem ipsum sit, consectetur adipiscing elit. Maecenas mi quam vulputate.</p>
+                        <h3>Peliputan</h3>
+                        <p>Peliputan adalah kegiatan jurnalisme atau media yang melibatkan pengumpulan, penyuntingan, dan penyampaian informasi atau berita melalui berbagai media seperti tulisan, foto, video, atau audio.</p>
                     </div>
                     <!-- End Single features -->
                 </div>
@@ -458,10 +458,9 @@
                 <div class="col-lg-4 col-md-6 col-12">
                     <!-- Start Single Service -->
                     <div class="single-service">
-                        <i class="icofont icofont-listening"></i>
+                        <i class="icofont  icofont-listening"></i>
                         <h4><a href="service-details.html">Peliputan</a></h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus dictum eros ut
-                            imperdiet. </p>
+                        <p >Peliputan adalah kegiatan jurnalisme atau media yang melibatkan pengumpulan, penyuntingan, dan penyampaian informasi atau berita melalui berbagai media seperti tulisan, foto, video, atau audio. </p>
                     </div>
                     <!-- End Single Service -->
                 </div>
@@ -469,9 +468,8 @@
                     <!-- Start Single Service -->
                     <div class="single-service">
                         <i class="icofont icofont-eye-alt"></i>
-                        <h4><a href="service-details.html">Publikasi</a></h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus dictum eros ut
-                            imperdiet. </p>
+                        <h4><a href="service-details.html">Pemberitaan dan Publikasi</a></h4>
+                        <p>Pemberitaan dan publikasi adalah proses menyebarkan informasi atau berita kepada khalayak melalui media cetak, elektronik, online, atau platform lainnya. </p>
                     </div>
                     <!-- End Single Service -->
                 </div>
@@ -479,7 +477,7 @@
                     <!-- Start Single Service -->
                     <div class="single-service">
                         <i class="icofont icofont-blood"></i>
-                        <h4><a href="service-details.html">Blood Transfusion</a></h4>
+                        <h4><a href="service-details.html">Editing Video</a></h4>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus dictum eros ut
                             imperdiet. </p>
                     </div>

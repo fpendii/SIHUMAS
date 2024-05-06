e92ee7cd836d887a4a40c2cb5d442712c17830c7
FerdiNurrahman <ferdi.nurrahman@mhs.politala.ac.id>
2024-05-07 02:20:31 +0800

    ngubah di jasa namanya doang sih daripada entar bentrok

--- resources/views/pages/pelanggan/pilih_jasa.blade.php
+++ resources/views/pages/pelanggan/pilih_jasa.blade.php
@@ -73,7 +73,7 @@
                                     <div class="row">
                                         <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
                                             <div class="stats-icon green mb-2">
-                                                <i class="iconly-boldAdd-User"></i>
+                                                <i class="iconly-boldimage"></i>
                                             </div>
                                         </div>
                                         <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
@@ -91,7 +91,7 @@
                                     <div class="row">
                                         <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
                                             <div class="stats-icon red mb-2">
-                                                <i class="iconly-boldBookmark"></i>
+                                                <i class="iconly-boldcamera"></i>
                                             </div>
                                         </div>
                                         <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">

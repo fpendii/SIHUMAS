160529f0c372ceaf90f2544edca99847f892aa47
FerdiNurrahman <ferdiart113@gmail.com>
2024-06-22 08:56:32 +0800

    Pas dan edit foto

--- app/Http/Controllers/Pegawai/PetugasController.php
+++ app/Http/Controllers/Pegawai/PetugasController.php
@@ -13,6 +13,8 @@ class PetugasController extends Controller
     public function index(){
         $PermohonanPeliputan = DB::table('pesanan')->join('jasa','pesanan.id_jasa','=','jasa.id_jasa')->where('jenis_jasa','=','peliputan')->get()->toArray();
         $PermohonanEditingVideo = DB::table('pesanan')->join('jasa','pesanan.id_jasa','=','jasa.id_jasa')->where('jenis_jasa','=','editing video')->get()->toArray();
+        $PermohonanEditFoto = DB::table('pesanan')->join('jasa','pesanan.id_jasa','=','jasa.id_jasa')->where('jenis_jasa','=','edit foto')->get()->toArray();
+        $PermohonanPasFoto = DB::table('pesanan')->join('jasa','pesanan.id_jasa','=','jasa.id_jasa')->where('jenis_jasa','=','pas foto')->get()->toArray();
 
 
         $data = [
@@ -21,6 +23,9 @@ class PetugasController extends Controller
             'level' => 'Petugas',
             'totalpeliputan' => count($PermohonanPeliputan),
             'totaleditingvideo' => count($PermohonanEditingVideo),
+            'totaleditfoto' => count($PermohonanEditFoto),
+            'totalpasfoto' => count($PermohonanPasFoto),
+
         ];
         return view('pages.petugas.beranda',$data);
 
--- resources/views/pages/petugas/beranda.blade.php
+++ resources/views/pages/petugas/beranda.blade.php
@@ -40,6 +40,42 @@
                         </div>
                     </div>
                     <div class="col-6 col-lg-3 col-md-6">
+                        <a href="{{ url('pegawai/pas-foto') }}" class="card-link">
+                            <div class="card">
+                                <div class="card-body px-4 py-4-5">
+                                    <div class="row">
+                                        <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
+                                            <div class="stats-icon red mb-2">
+                                                <i class="fas fa-camera"></i>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                                            <h6 class="text-muted font-semibold">Pas Foto</h6>
+                                            <h6 class="font-extrabold mb-0">{{$totalpasfoto}}</h6>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-6 col-lg-3 col-md-6">
+                        <div class="card">
+                            <div class="card-body px-4 py-4-5">
+                                <div class="row">
+                                    <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
+                                        <div class="stats-icon green mb-2">
+                                            <i class="fas fa-image"></i>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                                        <h6 class="text-muted font-semibold">Edit Foto</h6>
+                                        <h6 class="font-extrabold mb-0">{{$totaleditfoto}}</h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6 col-lg-3 col-md-6">
                         <div class="card">
                             <div class="card-body px-4 py-4-5">
                                 <div class="row">

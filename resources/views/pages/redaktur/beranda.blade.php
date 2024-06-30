429c8cb4eb2f8bdce1f0f05affd1a33ffe9ef408
FerdiNurrahman <ferdiart113@gmail.com>
2024-06-30 14:33:40 +0800

    selesai redaktur

--- resources/views/pages/redaktur/beranda.blade.php
+++ resources/views/pages/redaktur/beranda.blade.php
@@ -6,61 +6,29 @@
             <div class="col-12 col-lg-9">
                 <div class="row">
                     <div class="col-6 col-lg-3 col-md-6">
-                        <div class="card">
-                            <div class="card-body px-4 py-4-5">
-                                <div class="row">
-                                    <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start">
-                                        <div class="stats-icon green mb-2">
-                                            <i class="fa fa-video"></i>
+                        <a href="{{ url('redaktur/periksa_publikasi') }}" style="text-decoration: none; color: inherit;">
+                            <div class="card">
+                                <div class="card-body px-4 py-4-5">
+                                    <div class="row">
+                                        <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start">
+                                            <div class="stats-icon blue mb-2">
+                                                <i class="fas fa-bullhorn"></i>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                        <h6 class="text-muted font-semibold">Video Editing</h6>
-                                        {{-- <h6 class="font-extrabold mb-0">{{ $totaleditingvideo }}</h6> --}}
+                                        <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                                            <h6 class="text-muted font-semibold">Publikasi</h6>
+                                            <h6 class="font-extrabold mb-0">{{ $totalPublikasi }}</h6>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                    <div class="col-6 col-lg-3 col-md-6">
-                        <div class="card">
-                            <div class="card-body px-4 py-4-5">
-                                <div class="row">
-                                    <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start">
-                                        <div class="stats-icon blue mb-2">
-                                            <i class="fas fa-bullhorn"></i>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                        <h6 class="text-muted font-semibold">Peliputan</h6>
-                                        {{-- <h6 class="font-extrabold mb-0">{{ $totaleditingvideo }}</h6> --}}
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-6 col-lg-3 col-md-6">
-                        <div class="card">
-                            <div class="card-body px-4 py-4-5">
-                                <div class="row">
-                                    <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
-                                        <div class="stats-icon blue mb-2">
-                                            <i class="fas fa-bullhorn"></i>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                        <h6 class="text-muted font-semibold">Publikasi</h6>
-                                        <h6 class="font-extrabold mb-0">{{ $totalPublikasi }}</h6>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        </a>
                     </div>
                 </div>
             </div>
             <div class="col-12 col-lg-3 order-first order-lg-last">
                 <div class="card">
-                    <a href="{{ url('redaktur/profil') }}">
+                    <a href="{{ url('#') }}">
                         <div class="card-body py-4 px-4">
                             <div class="d-flex align-items-center">
                                 <div class="avatar avatar-xl">

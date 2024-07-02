82c6fe105dd5b4712e393db05259ceeb187ada5b
FerdiNurrahman <ferdiart113@gmail.com>
2024-07-02 10:12:31 +0800

    ada yang diubah sedikit

--- database/migrations/2024_03_05_072051_pesanan_migration.php
+++ database/migrations/2024_03_05_072051_pesanan_migration.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('link_mentahan',200)->nullable();
             $table->string('link_hasil',200)->nullable();
             $table->text('pesan');
-            $table->datetime('tenggat_pengerjaan');
+            $table->datetime('tenggat_pengerjaan')->nullable();
             $table->timestamps();
 
             $table->foreign('id_akun')->references('id_akun')->on('akun')->onDelete('cascade');

0e4d8e4d812e4934d6bbf4feec4347853d58bbc1
pendi <nurependi@mhs.politala.ac.id>
2024-07-04 17:57:10 +0800

    update halaman pilih layanan

--- database/migrations/2024_03_05_072051_pesanan_migration.php
+++ database/migrations/2024_03_05_072051_pesanan_migration.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('link_hasil',200)->nullable();
             $table->text('pesan');
             $table->date('tenggat_pengerjaan');
+            $table->char('unit',200)->nullable();
             $table->timestamps();
 
             $table->foreign('id_akun')->references('id_akun')->on('akun')->onDelete('cascade');

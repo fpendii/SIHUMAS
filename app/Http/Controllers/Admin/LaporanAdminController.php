44a5568d61abda692b22ecc064d0473d05a6d8f6
risda29 <risdar187@gmail.com>
2024-07-06 09:58:33 +0800

    fix laporan

--- app/Http/Controllers/Admin/LaporanAdminController.php
+++ app/Http/Controllers/Admin/LaporanAdminController.php
@@ -8,32 +8,20 @@ use Barryvdh\DomPDF\Facade\Pdf;
 
 class LaporanAdminController extends Controller
 {
-    public function index()
+    public function index()      
     {
         $data = [
             'title' => 'Laporan Bulanan | SIHUMAS',
-            'page' => 'Laporan Permohonan',
+            'page' => 'Laporan Bulanan',
             'level' => 'Admin',
             'Laporan' => DB::table('pesanan')
                 ->join('jasa', 'pesanan.id_jasa', '=', 'jasa.id_jasa')
                 ->join('akun', 'pesanan.id_akun', '=', 'akun.id_akun')
                 ->select(
-                    'jasa.jenis_jasa',
-                    'akun.nama',
-                    'pesanan.status',
-                    DB::raw('COUNT(pesanan.id_pesanan) as total'),
-                    DB::raw("SUM(CASE WHEN pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 WEEK) THEN 1 ELSE 0 END) as total_perminggu"),
-                    DB::raw("SUM(CASE WHEN pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 MONTH) THEN 1 ELSE 0 END) as total_perbulan"),
-                    DB::raw("SUM(CASE WHEN pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 YEAR) THEN 1 ELSE 0 END) as total_pertahun"),
-                    DB::raw("SUM(CASE WHEN pesanan.status = 'proses' AND pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 WEEK) THEN 1 ELSE 0 END) as proses_perminggu"),
-                    DB::raw("SUM(CASE WHEN pesanan.status = 'proses' AND pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 MONTH) THEN 1 ELSE 0 END) as proses_perbulan"),
-                    DB::raw("SUM(CASE WHEN pesanan.status = 'proses' AND pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 YEAR) THEN 1 ELSE 0 END) as proses_pertahun"),
-                    DB::raw("SUM(CASE WHEN pesanan.status = 'tolak' AND pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 WEEK) THEN 1 ELSE 0 END) as tolak_perminggu"),
-                    DB::raw("SUM(CASE WHEN pesanan.status = 'tolak' AND pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 MONTH) THEN 1 ELSE 0 END) as tolak_perbulan"),
-                    DB::raw("SUM(CASE WHEN pesanan.status = 'tolak' AND pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 YEAR) THEN 1 ELSE 0 END) as tolak_pertahun"),
-                    DB::raw("SUM(CASE WHEN pesanan.status = 'selesai' AND pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 WEEK) THEN 1 ELSE 0 END) as selesai_perminggu"),
-                    DB::raw("SUM(CASE WHEN pesanan.status = 'selesai' AND pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 MONTH) THEN 1 ELSE 0 END) as selesai_perbulan"),
-                    DB::raw("SUM(CASE WHEN pesanan.status = 'selesai' AND pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 YEAR) THEN 1 ELSE 0 END) as selesai_pertahun")
+                    'jasa.jenis_jasa', 
+                    'akun.nama', 
+                    'pesanan.status', 
+                    DB::raw('COUNT(pesanan.id_pesanan) as total')
                 )
                 ->groupBy('jasa.jenis_jasa', 'akun.nama', 'pesanan.status')
                 ->get()
@@ -43,34 +31,22 @@ class LaporanAdminController extends Controller
         return view('pages.admin.laporan_admin.laporan_admin', $data);
     }
 
-    // Metode cetakPDF ditambahkan di sini
     public function cetakPDF()
     {
         $data = [
             'title' => 'Laporan Bulanan | SIHUMAS',
-            'page' => 'Laporan ',
+            'page' => 'Laporan Bulanan',
             'level' => 'Admin',
             'Laporan' => DB::table('pesanan')
                 ->join('jasa', 'pesanan.id_jasa', '=', 'jasa.id_jasa')
                 ->join('akun', 'pesanan.id_akun', '=', 'akun.id_akun')
                 ->select(
-                    'jasa.jenis_jasa',
-                    'akun.nama',
-                    'pesanan.status',
-                    DB::raw('COUNT(pesanan.id_pesanan) as total'),
-                    DB::raw("SUM(CASE WHEN pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 WEEK) THEN 1 ELSE 0 END) as total_perminggu"),
-                    DB::raw("SUM(CASE WHEN pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 MONTH) THEN 1 ELSE 0 END) as total_perbulan"),
-                    DB::raw("SUM(CASE WHEN pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 YEAR) THEN 1 ELSE 0 END) as total_pertahun"),
-                    DB::raw("SUM(CASE WHEN pesanan.status = 'proses' AND pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 WEEK) THEN 1 ELSE 0 END) as proses_perminggu"),
-                    DB::raw("SUM(CASE WHEN pesanan.status = 'proses' AND pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 MONTH) THEN 1 ELSE 0 END) as proses_perbulan"),
-                    DB::raw("SUM(CASE WHEN pesanan.status = 'proses' AND pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 YEAR) THEN 1 ELSE 0 END) as proses_pertahun"),
-                    DB::raw("SUM(CASE WHEN pesanan.status = 'tolak' AND pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 WEEK) THEN 1 ELSE 0 END) as tolak_perminggu"),
-                    DB::raw("SUM(CASE WHEN pesanan.status = 'tolak' AND pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 MONTH) THEN 1 ELSE 0 END) as tolak_perbulan"),
-                    DB::raw("SUM(CASE WHEN pesanan.status = 'tolak' AND pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 YEAR) THEN 1 ELSE 0 END) as tolak_pertahun"),
-                    DB::raw("SUM(CASE WHEN pesanan.status = 'selesai' AND pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 WEEK) THEN 1 ELSE 0 END) as selesai_perminggu"),
-                    DB::raw("SUM(CASE WHEN pesanan.status = 'selesai' AND pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 MONTH) THEN 1 ELSE 0 END) as selesai_perbulan"),
-                    DB::raw("SUM(CASE WHEN pesanan.status = 'selesai' AND pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 YEAR) THEN 1 ELSE 0 END) as selesai_pertahun")
+                    'jasa.jenis_jasa', 
+                    'akun.nama', 
+                    'pesanan.status', 
+                    DB::raw('COUNT(pesanan.id_pesanan) as total')
                 )
+                ->where('pesanan.status', 'selesai')
                 ->groupBy('jasa.jenis_jasa', 'akun.nama', 'pesanan.status')
                 ->get()
                 ->toArray()
@@ -82,126 +58,5 @@ class LaporanAdminController extends Controller
 }
 
 
-    // Metode index sudah ada di sini      ini sum total perbulan minggu tahun
-    // public function index()
-    // {
-    //     $data = [
-    //         'title' => 'Laporan Bulanan | SIHUMAS',
-    //         'page' => 'Laporan Bulanan',
-    //         'level' => 'Koordinator',
-    //         'Laporan' => DB::table('pesanan')
-    //             ->join('jasa', 'pesanan.id_jasa', '=', 'jasa.id_jasa')
-    //             ->join('akun', 'pesanan.id_akun', '=', 'akun.id_akun')
-    //             ->select(
-    //                 'jasa.jenis_jasa',
-    //                 'akun.nama',
-    //                 'pesanan.status',
-    //                 DB::raw('COUNT(pesanan.id_pesanan) as total'),
-    //                 DB::raw("SUM(CASE WHEN pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 WEEK) THEN 1 ELSE 0 END) as total_perminggu"),
-    //                 DB::raw("SUM(CASE WHEN pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 MONTH) THEN 1 ELSE 0 END) as total_perbulan"),
-    //                 DB::raw("SUM(CASE WHEN pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 YEAR) THEN 1 ELSE 0 END) as total_pertahun"),
-    //                 DB::raw("SUM(CASE WHEN pesanan.status = 'proses' AND pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 WEEK) THEN 1 ELSE 0 END) as proses_perminggu"),
-    //                 DB::raw("SUM(CASE WHEN pesanan.status = 'proses' AND pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 MONTH) THEN 1 ELSE 0 END) as proses_perbulan"),
-    //                 DB::raw("SUM(CASE WHEN pesanan.status = 'proses' AND pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 YEAR) THEN 1 ELSE 0 END) as proses_pertahun"),
-    //                 DB::raw("SUM(CASE WHEN pesanan.status = 'tolak' AND pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 WEEK) THEN 1 ELSE 0 END) as tolak_perminggu"),
-    //                 DB::raw("SUM(CASE WHEN pesanan.status = 'tolak' AND pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 MONTH) THEN 1 ELSE 0 END) as tolak_perbulan"),
-    //                 DB::raw("SUM(CASE WHEN pesanan.status = 'tolak' AND pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 YEAR) THEN 1 ELSE 0 END) as tolak_pertahun"),
-    //                 DB::raw("SUM(CASE WHEN pesanan.status = 'selesai' AND pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 WEEK) THEN 1 ELSE 0 END) as selesai_perminggu"),
-    //                 DB::raw("SUM(CASE WHEN pesanan.status = 'selesai' AND pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 MONTH) THEN 1 ELSE 0 END) as selesai_perbulan"),
-    //                 DB::raw("SUM(CASE WHEN pesanan.status = 'selesai' AND pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 YEAR) THEN 1 ELSE 0 END) as selesai_pertahun")
-    //             )
-    //             ->groupBy('jasa.jenis_jasa', 'akun.nama', 'pesanan.status')
-    //             ->get()
-    //             ->toArray()
-    //     ];
-
-    //     return view('pages.koordinator.kelola_laporan.laporan', $data);
-    // }
-
-    // // Metode cetakPDF ditambahkan di sini
-    // public function cetakPDF()
-    // {
-    //     $data = [
-    //         'title' => 'Laporan Bulanan | SIHUMAS',
-    //         'page' => 'Laporan Bulanan',
-    //         'level' => 'Koordinator',
-    //         'Laporan' => DB::table('pesanan')
-    //             ->join('jasa', 'pesanan.id_jasa', '=', 'jasa.id_jasa')
-    //             ->join('akun', 'pesanan.id_akun', '=', 'akun.id_akun')
-    //             ->select(
-    //                 'jasa.jenis_jasa',
-    //                 'akun.nama',
-    //                 'pesanan.status',
-    //                 DB::raw('COUNT(pesanan.id_pesanan) as total'),
-    //                 DB::raw("SUM(CASE WHEN pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 WEEK) THEN 1 ELSE 0 END) as total_perminggu"),
-    //                 DB::raw("SUM(CASE WHEN pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 MONTH) THEN 1 ELSE 0 END) as total_perbulan"),
-    //                 DB::raw("SUM(CASE WHEN pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 YEAR) THEN 1 ELSE 0 END) as total_pertahun"),
-    //                 DB::raw("SUM(CASE WHEN pesanan.status = 'proses' AND pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 WEEK) THEN 1 ELSE 0 END) as proses_perminggu"),
-    //                 DB::raw("SUM(CASE WHEN pesanan.status = 'proses' AND pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 MONTH) THEN 1 ELSE 0 END) as proses_perbulan"),
-    //                 DB::raw("SUM(CASE WHEN pesanan.status = 'proses' AND pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 YEAR) THEN 1 ELSE 0 END) as proses_pertahun"),
-    //                 DB::raw("SUM(CASE WHEN pesanan.status = 'tolak' AND pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 WEEK) THEN 1 ELSE 0 END) as tolak_perminggu"),
-    //                 DB::raw("SUM(CASE WHEN pesanan.status = 'tolak' AND pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 MONTH) THEN 1 ELSE 0 END) as tolak_perbulan"),
-    //                 DB::raw("SUM(CASE WHEN pesanan.status = 'tolak' AND pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 YEAR) THEN 1 ELSE 0 END) as tolak_pertahun"),
-    //                 DB::raw("SUM(CASE WHEN pesanan.status = 'selesai' AND pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 WEEK) THEN 1 ELSE 0 END) as selesai_perminggu"),
-    //                 DB::raw("SUM(CASE WHEN pesanan.status = 'selesai' AND pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 MONTH) THEN 1 ELSE 0 END) as selesai_perbulan"),
-    //                 DB::raw("SUM(CASE WHEN pesanan.status = 'selesai' AND pesanan.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 YEAR) THEN 1 ELSE 0 END) as selesai_pertahun")
-    //             )
-    //             ->groupBy('jasa.jenis_jasa', 'akun.nama', 'pesanan.status')
-    //             ->get()
-    //             ->toArray()
-    //     ];
-
-    //     $pdf = PDF::loadView('pages.koordinator.kelola_laporan.cetak_laporan', $data);
-    //     return $pdf->download('laporan_bulanan.pdf');
-    // }
-
-
-
-    // public function index()      ini sum total aja
-    // {
-    //     $data = [
-    //         'title' => 'Laporan Bulanan | SIHUMAS',
-    //         'page' => 'Laporan Bulanan',
-    //         'level' => 'Koordinator',
-    //         'Laporan' => DB::table('pesanan')
-    //             ->join('jasa', 'pesanan.id_jasa', '=', 'jasa.id_jasa')
-    //             ->join('akun', 'pesanan.id_akun', '=', 'akun.id_akun')
-    //             ->select(
-    //                 'jasa.jenis_jasa', 
-    //                 'akun.nama', 
-    //                 'pesanan.status', 
-    //                 DB::raw('COUNT(pesanan.id_pesanan) as total')
-    //             )
-    //             ->groupBy('jasa.jenis_jasa', 'akun.nama', 'pesanan.status')
-    //             ->get()
-    //             ->toArray()
-    //     ];
-
-    //     return view('pages.koordinator.kelola_laporan.laporan', $data);
-    // }
-
-    // public function cetakPDF()
-    // {
-    //     $data = [
-    //         'title' => 'Laporan Bulanan | SIHUMAS',
-    //         'page' => 'Laporan Bulanan',
-    //         'level' => 'Koordinator',
-    //         'Laporan' => DB::table('pesanan')
-    //             ->join('jasa', 'pesanan.id_jasa', '=', 'jasa.id_jasa')
-    //             ->join('akun', 'pesanan.id_akun', '=', 'akun.id_akun')
-    //             ->select(
-    //                 'jasa.jenis_jasa', 
-    //                 'akun.nama', 
-    //                 'pesanan.status', 
-    //                 DB::raw('COUNT(pesanan.id_pesanan) as total')
-    //             )
-    //             ->groupBy('jasa.jenis_jasa', 'akun.nama', 'pesanan.status')
-    //             ->get()
-    //             ->toArray()
-    //     ];
-
-    //     $pdf = PDF::loadView('pages.koordinator.kelola_laporan.cetak_laporan', $data);
-    //     return $pdf->download('laporan_bulanan.pdf');
-    // }
 
 

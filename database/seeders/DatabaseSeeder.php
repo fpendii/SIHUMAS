5f1304c55c88ac6092ddd61550b894b51dad8ef2
fpendii <144331827+fpendii@users.noreply.github.com>
2025-01-22 20:28:38 +0800

    update sebelum hosting

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            akun_seeder::class
+            akun_seeder::class,
+            jasa_seeder::class
         ]);
     }
 }
